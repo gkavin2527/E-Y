68012c031274b75f14dda1748005a7ac05d32564
Kavin G <gkavin446@gmail.com>
2025-12-04 04:20:29 +0000

    i mean chagne that navbar thing

--- src/components/layout/site-header.tsx
+++ src/components/layout/site-header.tsx
@@ -164,14 +164,12 @@ export function SiteHeader() {
   
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center">
-        <div className="flex-1 justify-start">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        <div className="flex gap-6 md:gap-10">
             <Link href="/">
                 <span className="text-2xl font-bold font-headline tracking-wide">E&Y</span>
             </Link>
-        </div>
-        <div className="flex flex-1 justify-center">
-          <MainNav />
+            <MainNav />
         </div>
         <div className="flex flex-1 items-center justify-end space-x-1">
             <SearchDialog />
